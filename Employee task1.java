d050595436ab87143e307f4219689963287f3623
priyan400 <105925052+priyan400@users.noreply.github.com>
2022-05-20 18:09:10 +0530

    intialstage
    creating the details for the Employee

---
+++ Employee task1.java
@@ -0,0 +1,42 @@
+package org.employee;
+
+public class Employee {
+	private void employeeId() {
+		// TODO Auto-generated method stub
+		System.out.println("EMP0001");	
+	}
+	private void employeeName() {
+		// TODO Auto-generated method stub
+		System.out.println("PRIYARAJKUMAR");
+	}
+	private void employeeDateOfBirth() {
+		// TODO Auto-generated method stub
+		System.out.println("14/06/1992");
+
+	}
+    private void employeeNumber() {
+		// TODO Auto-generated method stub
+    System.out.println("7010830270");
+	}
+    private void employeeEmail() {
+		// TODO Auto-generated method stub
+    System.out.println("prkpriyan@gmail.com");
+	}
+    
+    private void employeeAddress() {
+		// TODO Auto-generated method stub
+    System.out.println("Koodal Nagar,Madurai-625018");
+	}
+    public static void main(String[] args) {
+	
+    Employee b = new Employee();
+    b.employeeId();
+    b.employeeName();
+    b.employeeDateOfBirth();
+    b.employeeNumber();
+    b.employeeEmail();
+    b.employeeAddress();
+ 
+    
+	}
+}
